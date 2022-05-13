bfc739d5a4e2b76dcb4f04eb79f9cd4bfe4ef0cd
Matheus-Rezende <matheus-rezende-@hotmail.com>
2022-05-12 21:42:55 -0300

    Aula 2: criando o bottomNavigationBar

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/pages/home_page.dart';
+import 'package:youtube/pages/library_page.dart';
+import 'package:youtube/pages/shorts_page.dart';
+import 'package:youtube/pages/subs_page.dart';
 
 class Home extends StatefulWidget {
   const Home({ Key? key }) : super(key: key);
@@ -8,6 +12,16 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _pageIndex = 0;
+  
+  final pages = [
+    const HomePage(),
+    const ShortsPage(),
+    const SubscriptionsPage(),
+    const LibraryPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,8 +61,63 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      body: Container(
-
+      body: Center(
+        child: pages[_pageIndex],
+      ),
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          indicatorColor: const Color.fromARGB(255, 255, 0, 0).withOpacity(0.7)
+        ),
+        child: NavigationBar(
+          backgroundColor: const Color.fromARGB(255, 245, 245, 245),
+          animationDuration: const Duration(seconds: 1),
+          height: 60,
+          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+          selectedIndex: _pageIndex,
+          onDestinationSelected: (int newIndex){
+            setState(() {
+              _pageIndex = newIndex;
+            });
+          },
+          destinations: const [
+            NavigationDestination(
+              selectedIcon: Icon(
+                Icons.home, 
+              ),
+              icon: Icon(
+                Icons.home_outlined,
+              ),
+              label: 'Home',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(
+                  Icons.play_arrow_rounded
+                ),
+              icon: Icon(
+                  Icons.play_arrow_outlined,
+                ),
+              label: 'Shorts',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(
+                Icons.subscriptions,
+              ),
+              icon: Icon(
+                Icons.subscriptions_outlined,
+              ),
+              label: 'Inscrições',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(
+                Icons.video_library_rounded,
+              ),
+              icon: Icon(
+                Icons.video_library_outlined, 
+              ),
+              label: 'Biblioteca',
+            )
+          ],
+        ),
       )
     );
   }
